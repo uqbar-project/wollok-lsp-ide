1a6211ba7ac58e16f9cb25ec9ddd33fd9539f643
palumbon <nahuel.palumbo@gmail.com>
2023-04-16 21:34:56 +0200

    Adding expected command tests

--- client/src/test/commands.test.ts
+++ client/src/test/commands.test.ts
@@ -1,16 +1,27 @@
 import * as assert from 'assert'
 import { ShellExecution, Task, Uri } from 'vscode'
-import { startRepl } from '../commands'
+import { runAllTests, runProgram, runTests, startRepl } from '../commands'
 import { activate, getDocumentURI, getFolderURI } from './helper'
 
 suite('Should run commands', () => {
   const folderURI = getFolderURI()
   const pepitaURI = getDocumentURI('pepita.wlk')
 
+  test('run program', async () => {
+    await testCommand(pepitaURI, () => runProgram('file.program'), ` run 'file.program' --skipValidations -p ${folderURI.fsPath}`)
+  })
+
+  test('run tests', async () => {
+    await testCommand(pepitaURI, () => runTests('tests."tests de pepita"."something"'), ` test 'tests.\\"tests de pepita\\".\\"something\\"' --skipValidations -p ${folderURI.fsPath}`)
+  })
+
+  test('run all tests', async () => {
+    await testCommand(pepitaURI, runAllTests, ` test -p ${folderURI.fsPath} --skipValidations`)
+  })
+
   test('repl on current file', async () => {
     await testCommand(pepitaURI, startRepl, ` repl ${pepitaURI.fsPath} -p ${folderURI.fsPath}`)
   })
-
 })
 
 async function testCommand(docUri: Uri, command: () => Task, expectedCommand: string) {
