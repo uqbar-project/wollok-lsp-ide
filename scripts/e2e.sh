4bd9b2989271e28591542e856c51216abe16ece6
Fernando Dodino <fernando.dodino@gmail.com>
2023-10-22 20:46:16 -0300

    Testing old issue fix

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+export ELECTRON_RUN_AS_NODE=1
 export CODE_TESTS_PATH="$(pwd)/client/out/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
 
-node "$(pwd)/client/out/test/runTest"
+node "$(pwd)/client/out/test/runTest" --ms-enable-electron-run-as-node 
