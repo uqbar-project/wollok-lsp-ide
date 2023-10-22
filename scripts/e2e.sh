02044720e620e2bd026698c507b47142a8c16330
Fernando Dodino <fernando.dodino@gmail.com>
2023-10-22 20:41:24 -0300

    Using old version of VSC

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-
 export CODE_TESTS_PATH="$(pwd)/client/out/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
 
-node "$(pwd)/client/out/test/runTest" --env DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" --network=host
+node "$(pwd)/client/out/test/runTest"
