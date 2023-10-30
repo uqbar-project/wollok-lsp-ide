5cc3e30a61bddcbf8481390bc7b97bc700fbbe42
Fernando Dodino <fernando.dodino@gmail.com>
2023-10-29 23:37:18 -0300

    Enhance README

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,13 +5,21 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-VSIX_URL="https://github.com/uqbar-project/wollok-lsp-ide/releases/download/v$TAG/wollok-lsp-ide-$TAG.vsix"
-wget_output=$(wget -q "$VSIX_URL")
+BASE_URL="https://github.com/uqbar-project/wollok-lsp-ide/releases/download/v$TAG/"
+VSIX_FILE="wollok-lsp-ide-$TAG.vsix"
+wget_output=$(wget -q -O $VSIX_FILE "$BASE_URL$VSIX_FILE")
 if [ $? -ne 0 ]; then
-  echo "publish: problems while trying to get $VSIX_URL."
+  echo "publish: problems while trying to get $BASE_URL$VSIX_FILE."
   echo "Please make sure it is a valid tag and you followed convention rules: if version is 0.1.0 the tag should be v0.1.0"
   exit 2
 fi
 
+if ! grep -q -F $TAG "CHANGELOG.md"; then
+  echo "publish: version $TAG missing in CHANGELOG.md"
+  exit 3
+fi
+
+echo "Everything ok. Publishing $TAG in Marketplace..."
 npm i -g vsce
 vsce publish
+echo "Check new version in https://marketplace.visualstudio.com/manage/publishers/uqbar"
