3cddbcb0b7b9fa3c5347794a9b79d44d7ad8f1d9
ivojawer <ivojawerbaum@gmail.com>
2024-01-04 02:10:10 -0300

    revert test filter

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -66,7 +66,7 @@ async function runTests(): Promise<void> {
   const testsRoot = __dirname
 
   return new Promise((resolve, reject) => {
-    glob('rename.test.js', { cwd: testsRoot }, (err, files) => {
+    glob('*.test.js', { cwd: testsRoot }, (err, files) => {
       if (err) {
         return reject(err)
       }
