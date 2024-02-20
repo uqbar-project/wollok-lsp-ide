1729940d74711cc96176f08b1b668c0a34d89244
ivojawer <ivojawerbaum@gmail.com>
2024-02-20 17:37:58 -0300

    references

--- server/src/functionalities/references.ts
+++ server/src/functionalities/references.ts
@@ -1,8 +1,20 @@
 import { Location, ReferenceParams } from 'vscode-languageserver'
-import { Environment } from 'wollok-ts'
+import { Environment, Method, Node, Reference, Send, Singleton } from 'wollok-ts'
+import { cursorNode, nodeToLocation } from '../utils/text-documents'
+import { targettingAt } from '../utils/vm/wollok'
 
 export const references = (environment: Environment) => (params: ReferenceParams): Location[] | null => {
+  const node = cursorNode(environment, params.position, params.textDocument)
 
-
-  return [Location.create(params.textDocument.uri, { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } })]
+  return environment.descendants.filter(
+    node.is(Method) ?
+      mayExecute(node) :
+      targettingAt(node)
+  ).map(nodeToLocation)
 }
+
+const mayExecute = (method: Method) => (aNode: Node) =>
+  aNode.is(Send) &&
+  aNode.message === method.name &&
+  // exclude cases where a message is sent to a different singleton
+  !(aNode.receiver.is(Reference) && aNode.receiver.target?.is(Singleton) && aNode.receiver.target !== method.parent)
