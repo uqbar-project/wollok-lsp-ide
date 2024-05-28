12acec8d6a1be388bb5ad197edc87897bc24e7fe
ivojawer <ivojawerbaum@gmail.com>
2024-05-28 01:05:35 -0300

    log references error

--- server/src/functionalities/references.ts
+++ server/src/functionalities/references.ts
@@ -1,10 +1,14 @@
 import { Location, ReferenceParams } from 'vscode-languageserver'
 import { Environment, Method, mayExecute, targettingAt } from 'wollok-ts'
 import { cursorNode, nodeToLocation } from '../utils/text-documents'
+import { logger } from '../utils/logger'
 
 export const references = (environment: Environment) => (params: ReferenceParams): Location[] | null => {
   const node = cursorNode(environment, params.position, params.textDocument)
-  if (!node) throw new Error('Could not find selection node')
+  if (!node) {
+    logger.error('✘ Could not find the node to search references for')
+    return null
+  }
   return environment.descendants.filter(
     node.is(Method) ?
       mayExecute(node) :
