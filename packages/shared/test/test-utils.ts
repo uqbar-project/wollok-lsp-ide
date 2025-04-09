42820e3781fd617a0a65d9a6e060e0d2e2cc76f2
ivojawer <ivojawerbaum@gmail.com>
2025-04-09 18:59:27 -0300

    Revert "refactor tests"
    This reverts commit dce2f82207db04088cc32dd21a52f58b28a99d42.

--- packages/shared/test/test-utils.ts
+++
@@ -1,8 +0,0 @@
-export function continueAfter(condition: () => boolean): Promise<void> {
-  return new Promise(function (resolve, _reject) {
-      (function waitForCondition(){
-          if (condition()) return resolve()
-          setTimeout(waitForCondition, 30)
-      })()
-  })
-}
